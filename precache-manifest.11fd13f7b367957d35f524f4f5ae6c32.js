a4f10677e2f40c639d544b1f2fde00ca645cacb9
mohits Projects <mohit5150515@gmail.com>
2020-12-25 22:18:18 +0530

    Updates

---
+++ precache-manifest.11fd13f7b367957d35f524f4f5ae6c32.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868ec8a1183a8b779c2a586e3a737579",
+    "url": "/iPod-pocketPod/index.html"
+  },
+  {
+    "revision": "442aed00fc6ade65a133",
+    "url": "/iPod-pocketPod/static/css/main.a2775053.chunk.css"
+  },
+  {
+    "revision": "b9bf6ea77e1534c0f1da",
+    "url": "/iPod-pocketPod/static/js/2.fd965c30.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/iPod-pocketPod/static/js/2.fd965c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442aed00fc6ade65a133",
+    "url": "/iPod-pocketPod/static/js/main.248fda12.chunk.js"
+  },
+  {
+    "revision": "65945e7a3f2b02b3c749",
+    "url": "/iPod-pocketPod/static/js/runtime-main.f88fa259.js"
+  },
+  {
+    "revision": "21ccb044688108d436d7394eccb7ad75",
+    "url": "/iPod-pocketPod/static/media/Scam-1992BGM.21ccb044.mp3"
+  },
+  {
+    "revision": "58a54a9fddc87484f693ab2483964acf",
+    "url": "/iPod-pocketPod/static/media/Tum-Tak.58a54a9f.mp3"
+  },
+  {
+    "revision": "dbfc11151f107f911be98961e051bbba",
+    "url": "/iPod-pocketPod/static/media/babyYoda.dbfc1115.jpg"
+  },
+  {
+    "revision": "ab7c69c2c47e3cbd52c55486514eb1d3",
+    "url": "/iPod-pocketPod/static/media/earth.ab7c69c2.jpg"
+  },
+  {
+    "revision": "d1ce8e3905563875c7811bf6d98f7d14",
+    "url": "/iPod-pocketPod/static/media/earth2020.d1ce8e39.jpg"
+  },
+  {
+    "revision": "6acd99a0663228293e20f760e3386e01",
+    "url": "/iPod-pocketPod/static/media/game.6acd99a0.png"
+  },
+  {
+    "revision": "b7bdadb2a1ba1f90240db5ba7258003d",
+    "url": "/iPod-pocketPod/static/media/joker.b7bdadb2.png"
+  },
+  {
+    "revision": "62a0c3589aeb5cc5f03c05b7c126cc24",
+    "url": "/iPod-pocketPod/static/media/pirates-jack-bgm.62a0c358.mp3"
+  },
+  {
+    "revision": "71585cdac2979c30a67e351d07f9b996",
+    "url": "/iPod-pocketPod/static/media/song1Img.71585cda.jpg"
+  },
+  {
+    "revision": "472698ac216dcec9316a96eafb22a4ea",
+    "url": "/iPod-pocketPod/static/media/song2Img.472698ac.jpg"
+  },
+  {
+    "revision": "b03f6054a06fbfc046c353ebe59d4cce",
+    "url": "/iPod-pocketPod/static/media/song3Img.b03f6054.jpg"
+  }
+]);
